173bffb0c83312ce6f2d4232ae7cc0b4a68c3684
iancrose <57683003+iancrose@users.noreply.github.com>
2019-11-21 14:53:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ tabtitle='Dash Playground'
 ########### Load the data
 df = pd.read_csv('https://ianrosewrites.com/1011010/days.csv')
 
-trace1 = go.Bar(x=df['daynum'], y=df['yes'], name='Counted')
-trace2 = go.Bar(x=df['daynum'], y=df['no'], name='Not Counted')
-
 pv = pd.pivot_table(df, index=['wknum'], columns=["daywk"], values=['yes'], fill_value=0)
 
 tracemo = go.Bar(x=pv.index, y=pv[('yes', '\'Mon\'')], name='Monday')
@@ -33,38 +30,6 @@ app.title=tabtitle
 ########### Set up the layout
 app.layout = html.Div([
     html.H1(children='Daily Logged Time'),
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['daywk'] == i]['daynum'],
-                    y=df[df['daywk'] == i]['yes'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.daywk.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'title': 'Day #'},
-                yaxis={'title': 'Hours Logged'},
-                margin={'l': 0, 'b': 0, 't': 0, 'r': 0},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    ), 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [trace1, trace2],
-            'layout':
-            go.Layout(barmode='stack')
-        }), 
     dcc.Graph(
         id='example-graph-2',
         figure={
